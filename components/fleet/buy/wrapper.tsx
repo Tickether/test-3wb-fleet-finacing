ec8e7ee0f7e6bf37acd95680cffcba6fd8af7f2c
Tickether <tickether@gmail.com>
2025-07-15 20:23:43 +0000

    Update wrapper.tsx

--- components/fleet/buy/wrapper.tsx
+++ components/fleet/buy/wrapper.tsx
@@ -296,7 +296,7 @@ export function Wrapper() {
                                         size="icon"
                                         className="h-8 w-8 shrink-0 rounded-full"
                                         onClick={isFractionsMode ? increaseFractions : increase}
-                                        disabled={isFractionsMode ? fractions >= 50 : amount >= 100}
+                                        disabled={isFractionsMode ? fractions >= 50 : amount >= 150}
                                     >
                                         <Plus />
                                         <span className="sr-only">Increase</span>
